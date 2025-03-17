161be98c420c718e58c99cd40b1da15d9dbe3a4a
CandyCandyCandy <161817534+BruCandy@users.noreply.github.com>
2025-03-17 15:34:21 +0900

    update count9 count99

--- count9.v
+++ count9.v
@@ -1,36 +1,36 @@
 module count9 (
-    input  wire i_clk,
-    input  wire i_rst,
+    input  wire       i_clk,
+    input  wire       i_rst,
     output wire [7:0] o_seg,
-    output wire [3:0]o_dig
+    output wire [3:0] o_dig
 );
 
     parameter WAIT = 27_000_000; // tang nano 9kは27MHz
     parameter BITS = 25;
-    parameter MAX = 9;
+    parameter MAX  = 9;
 
     reg [BITS-1:0]  r_wait = 0;
-    reg [3:0]   r_cnt = 0;
-    reg [7:0]   r_seg = 8'b11111100;
+    reg [3:0]       r_cnt = 0;
+    reg [7:0]       r_seg = 8'b11111100;
 
     wire w_rst;
 
     assign o_dig = 4'b1110; // 1桁目に表示
     assign o_seg = r_seg;
-    assign w_rst = ~ i_rst;
+    assign w_rst = ~ i_rst; // Tang Nano 9Kのボタンは負論理であるため
 
     always @(posedge i_clk or posedge w_rst) begin
         if (w_rst) begin
             r_wait <= 0;
-            r_cnt <= 0;
-            r_seg <= 8'b11111100;
+            r_cnt  <= 0;
+            r_seg  <= 8'b11111100;
         end else begin
             if (r_cnt == MAX+1) begin
                 r_cnt <= 0;
             end else begin
                 if (r_wait == WAIT-1) begin
                     r_wait <= 0;
-                    r_cnt <= r_cnt + 1'b1;
+                    r_cnt  <= r_cnt + 1'b1;
                 end else begin
                     r_wait <= r_wait + 1'b1;
                 end
--- count99.v
+++ count99.v
@@ -1,55 +1,62 @@
 module count99 (
-    input wire i_clk,
-    input wire i_rst,
+    input  wire       i_clk,
+    input  wire       i_rst,
     output wire [7:0] o_seg,
     output wire [3:0] o_dig
 );
 
-    parameter WAIT = 27_000_000; // tang nano 9kは27MHz
-    parameter BITS = 25;
-    parameter MAX  = 99;
+    parameter WAIT       = 27_000_000; // tang nano 9kは27MHz
+    parameter BITS       = 25;
+    parameter MAX        = 99;
     parameter TIMER_WAIT = 27_000;
 
     reg [BITS-1:0]  r_wait = 0;
-    reg [6:0]   r_cnt = 0;
-    reg [7:0]   r_seg1 = 8'b11111100;
-    reg [7:0]   r_seg2 = 8'b11111100;
-    reg [1:0]   r_dig = 0;
+    reg [6:0]       r_cnt  = 0;
+    reg             r_dig  = 0;
+    reg [7:0]       r_seg  [0:9];
 
     wire       w_rst;
-    wire       w_overflow;
+    wire       w_tick;
     wire [3:0] w_ones;
     wire [3:0] w_tens;
+
+    assign w_rst  = ~ i_rst; // Tang Nano 9Kのボタンは負論理であるため
+    assign w_ones = r_cnt % 10;
+    assign w_tens = (r_cnt / 10) % 10;
+
+    assign o_dig = w_rst ? 4'b1100 : 
+                   r_dig ? 4'b1101 : 4'b1110;
+    assign o_seg = r_dig ? r_seg[w_tens] : r_seg[w_ones];
+
+    initial begin
+        r_seg[0] = 8'b11111100;
+        r_seg[1] = 8'b01100000;
+        r_seg[2] = 8'b11011010;
+        r_seg[3] = 8'b11110010;
+        r_seg[4] = 8'b01100110;
+        r_seg[5] = 8'b10110110;
+        r_seg[6] = 8'b10111110;
+        r_seg[7] = 8'b11100000;
+        r_seg[8] = 8'b11111110;
+        r_seg[9] = 8'b11110110;
+    end
 
     timer #(
         .TIMER_WAIT(TIMER_WAIT)
     ) timer (
-        .i_rst(w_rst),
-        .i_clk(i_clk),
-        .o_overflow(w_overflow)
+        .i_rst  (w_rst ),
+        .i_clk  (i_clk ),
+        .o_tick (w_tick)
     );
-
-    assign w_rst  = ~ i_rst;
-    assign w_ones = r_cnt % 10;
-    assign w_tens = (r_cnt / 10) % 10;
-
-    assign o_dig = r_dig ? 4'b1101 : 4'b1110;
-    assign o_seg = r_dig ? r_seg2 : r_seg1;
 
     always @(posedge i_clk or posedge w_rst) begin
         if (w_rst) begin
             r_wait <= 0;
-            r_cnt <= 0;
-            r_seg1 <= 8'b11111100;
-            r_seg2 <= 8'b11111100;
-            r_dig <= 0;
+            r_cnt  <= 0;
+            r_dig  <= 0;
         end else begin
-            if (w_overflow) begin
-                if (r_dig == 1) begin
-                    r_dig <= 0;
-                end else begin
-                    r_dig <= 1;
-                end
+            if (w_tick) begin
+                r_dig <= ~r_dig;
             end
 
             if (r_cnt == MAX+1) begin
@@ -57,37 +64,11 @@ module count99 (
             end else begin
                 if (r_wait == WAIT-1) begin
                     r_wait <= 0;
-                    r_cnt <= r_cnt + 1'b1;
+                    r_cnt  <= r_cnt + 1'b1;
                 end else begin
                     r_wait <= r_wait + 1'b1;
                 end
             end
-
-            case (w_ones)
-                0 : r_seg1 <= 8'b11111100;
-                1 : r_seg1 <= 8'b01100000;
-                2 : r_seg1 <= 8'b11011010;
-                3 : r_seg1 <= 8'b11110010;
-                4 : r_seg1 <= 8'b01100110;
-                5 : r_seg1 <= 8'b10110110;
-                6 : r_seg1 <= 8'b10111110;
-                7 : r_seg1 <= 8'b11100000;
-                8 : r_seg1 <= 8'b11111110;
-                9 : r_seg1 <= 8'b11110110;
-            endcase
-
-            case (w_tens) 
-                0 : r_seg2 <= 8'b11111100;
-                1 : r_seg2 <= 8'b01100000;
-                2 : r_seg2 <= 8'b11011010;
-                3 : r_seg2 <= 8'b11110010;
-                4 : r_seg2 <= 8'b01100110;
-                5 : r_seg2 <= 8'b10110110;
-                6 : r_seg2 <= 8'b10111110;
-                7 : r_seg2 <= 8'b11100000;
-                8 : r_seg2 <= 8'b11111110;
-                9 : r_seg2 <= 8'b11110110;
-            endcase
         end
     end
 
@@ -98,27 +79,23 @@ endmodule
 module timer (
     input  wire i_rst,
     input  wire i_clk,
-    output wire o_overflow 
+    output wire o_tick 
 );
 
     parameter TIMER_WAIT = 27_000; // 1ms間隔で出力
 
     reg [14:0] r_cnt = 0;
-    reg        r_overflow = 0;
 
-    assign o_overflow = r_overflow;
+    assign o_tick = (r_cnt == TIMER_WAIT-1);
 
     always @(posedge i_clk) begin
         if (i_rst) begin
-            r_cnt <= 0;
-            r_overflow <= 0;
+            r_cnt  <= 0;
         end else begin
             if (r_cnt == TIMER_WAIT-1) begin
-                r_cnt <= 0;
-                r_overflow <= 1;
+                r_cnt  <= 0;
             end else begin
-                r_cnt <= r_cnt + 1'b1;
-                r_overflow <= 0;
+                r_cnt  <= r_cnt + 1'b1;
             end
         end
     end
